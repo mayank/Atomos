3337c936f08f37173525b19380b929ce38334d7a
Mayank Mishra <mayankmishra@197NODMB18174.local>
2018-06-28 23:54:47 +0530

    updated oprt

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/', function(req, res){
   res.render('new');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(80, function(){
+  console.log('listening on 80');
 });
 
 
